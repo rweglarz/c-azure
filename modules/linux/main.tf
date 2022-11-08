5ff4ae8e28de2eef99998cf4f4ee895539b7571d
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-11-08 18:36:41 +0100

    feat: modules/linux add simple module for building instance

---
+++ modules/linux/main.tf
@@ -0,0 +1,62 @@
+resource "azurerm_public_ip" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+
+resource "azurerm_network_interface" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_ip_address
+    public_ip_address_id          = azurerm_public_ip.this.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.size
+
+  network_interface_ids = [azurerm_network_interface.this.id]
+
+  os_disk {
+    name                 = var.name
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+
+  admin_username                  = var.username
+  admin_password                  = var.password
+  disable_password_authentication = false
+
+  admin_ssh_key {
+    username   = "ubuntu"
+    public_key = var.public_key
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "this" {
+  count = var.security_group != null ? 1 : 0
+
+  network_interface_id      = azurerm_network_interface.this.id
+  network_security_group_id = var.security_group
+}
+
+
---
+++ modules/linux/variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+variable "private_ip_address" {
+  type = string
+}
+
+variable "username" {
+  type    = string
+  default = "ubuntu"
+}
+variable "password" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "resource_group_name" {
+  type = string
+}
+
+variable "size" {
+  type    = string
+  default = "Standard_DS1_v2"
+}
+
+variable "public_key" {
+  type    = string
+  default = null
+}
+
+variable "security_group" {
+  type    = string
+  default = null
+}
