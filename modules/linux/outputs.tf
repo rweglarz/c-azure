7d1b11dc872e33ba084405003253b0ede04fb704
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-11-29 20:37:29 +0100

    feat: modules/linux output private ip address

--- modules/linux/outputs.tf
+++ modules/linux/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = azurerm_public_ip.this.ip_address
 }
+
+output "private_ip_address" {
+  value = azurerm_network_interface.this.ip_configuration[0].private_ip_address
+}
