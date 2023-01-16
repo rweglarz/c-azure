f0628a8fffafb0a59a9f4d270bdec2515dd347c1
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-01-16 22:11:42 +0100

    feat: vmss: deploy two app-insights for cn and vmss

--- vmss/app-insights.tf
+++ vmss/app-insights.tf
@@ -1,12 +1,24 @@
-resource "azurerm_application_insights" "this" {
-  name                = "${var.name}-app-insights"
+resource "azurerm_application_insights" "vmss" {
+  name                = "${var.name}-app-insights-vmss"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "other"
 }
 
+resource "azurerm_application_insights" "cn" {
+  name                = "${var.name}-app-insights-cn"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "other"
+}
+
+
+
 output "application_insights_instrumentation_key" {
   sensitive = true
 
-  value = azurerm_application_insights.this.instrumentation_key
+  value = {
+    cn   = azurerm_application_insights.cn.instrumentation_key
+    vmss = azurerm_application_insights.vmss.instrumentation_key
+  }
 }
