789b002b45721ed6cd81d6f07b2bf7eeb67505e7
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-01-20 16:35:59 +0100

    chore: vwan: move sdwan locals to common-locals

--- vwan/common-locals.tf
+++ vwan/common-locals.tf
@@ -1,5 +1,19 @@
 locals {
   subnet_prefix_length = 28
+  vnet_address_space = {
+    ipsec_hub1   = [cidrsubnet(var.hub1_cidr, 4, 2)]
+    ipsec_hub2   = [cidrsubnet(var.hub2_cidr, 4, 2)]
+    ipsec_spoke1 = [cidrsubnet(var.ext_spokes_cidr, 4, 2)]
+  }
+  public_interface_ids = {
+    hub2_sdwan_fw1  = one([for k, v in module.hub2_sdwan_fw1.interfaces : v.ip_configuration[0].public_ip_address_id if length(regexall("internet$", v.name)) > 0])
+    hub2_sdwan_fw2  = one([for k, v in module.hub2_sdwan_fw2.interfaces : v.ip_configuration[0].public_ip_address_id if length(regexall("internet$", v.name)) > 0])
+    sdwan_spoke1_fw = one([for k, v in module.sdwan_spoke1_fw.interfaces : v.ip_configuration[0].public_ip_address_id if length(regexall("internet$", v.name)) > 0])
+  }
+  public_interface_names = {
+    for k, v in local.public_interface_ids : k => element(split("/", v), length(split("/", v)) - 1)
+  }
+
   public_ip = {
     aws_fw1 = [
       one([for k, v in module.vm-fw-1.public_ips : v if length(regexall("isp1", k)) > 0]),
--- vwan/outputs-sdwan.tf
+++ vwan/outputs-sdwan.tf
@@ -1,15 +1,3 @@
-locals {
-  public_interface_ids = {
-    hub2_sdwan_fw1  = one([for k, v in module.hub2_sdwan_fw1.interfaces : v.ip_configuration[0].public_ip_address_id if length(regexall("internet$", v.name)) > 0])
-    hub2_sdwan_fw2  = one([for k, v in module.hub2_sdwan_fw2.interfaces : v.ip_configuration[0].public_ip_address_id if length(regexall("internet$", v.name)) > 0])
-    sdwan_spoke1_fw = one([for k, v in module.sdwan_spoke1_fw.interfaces : v.ip_configuration[0].public_ip_address_id if length(regexall("internet$", v.name)) > 0])
-  }
-  public_interface_names = {
-    for k, v in local.public_interface_ids : k => element(split("/", v), length(split("/", v)) - 1)
-  }
-}
-
-
 data "azurerm_public_ip" "sdwan_ips" {
   for_each            = local.public_interface_names
   name                = each.value
