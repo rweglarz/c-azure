e3f253f41cb60bf14b1805a27902192b116df990
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-01-20 16:03:39 +0100

    feat: module/vnet: add vnet module

---
+++ modules/vnet/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_virtual_network" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_space       = var.address_space
+}
+
+resource "azurerm_subnet" "this" {
+  for_each = var.subnets
+
+  name                 = "${var.name}-${each.key}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = each.value.address_prefixes
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  for_each = { for k, v in var.subnets : k => v if lookup(v, "network_security_group_id", "")!="" }
+
+  subnet_id                 = azurerm_subnet.this[each.key].id
+  network_security_group_id = each.value.network_security_group_id
+}
---
+++ modules/vnet/variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+
+}
+
+variable "resource_group_name" {
+
+}
+
+variable "location" {
+
+}
+
+variable "subnets" {
+
+}
+
+variable "address_space" {
+}
