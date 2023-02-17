7309ce5f14b02c818c08c55b2ec06d4f26c20622
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-02-17 18:07:11 +0100

    fix: plugin: simplify routing

--- plugin/route.tf
+++ plugin/route.tf
@@ -1,107 +1,93 @@
-resource "azurerm_route_table" "all" {
-  name                = "${var.name}-all"
+resource "azurerm_route_table" "all_via_inbound" {
+  name                = "${var.name}-all-via-inbound"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
-resource "azurerm_route" "all_appgw_ext" {
-  name                   = "appgw_ext"
+resource "azurerm_route" "all_via_inbound" {
+  for_each = {
+    appgw_ext = azurerm_virtual_network.appgw_ext.address_space[0]
+    appgw_int = azurerm_virtual_network.appgw_int.address_space[0]
+    w1        = azurerm_virtual_network.w1.address_space[0]
+    w2        = azurerm_virtual_network.w2.address_space[0]
+  }
+  name                   = each.key
   resource_group_name    = azurerm_resource_group.rg.name
-  route_table_name       = azurerm_route_table.all.name
-  address_prefix         = azurerm_virtual_network.appgw_ext.address_space[0]
+  route_table_name       = azurerm_route_table.all_via_inbound.name
+  address_prefix         = each.value
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = var.inb_int_lb
+}
+
+resource "azurerm_route_table" "all_via_ewo" {
+  name                = "${var.name}-all-via-ewo"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_route" "all_via_ewo" {
+  for_each = {
+    appgw_ext = azurerm_virtual_network.appgw_ext.address_space[0]
+    appgw_int = azurerm_virtual_network.appgw_int.address_space[0]
+    w1        = azurerm_virtual_network.w1.address_space[0]
+    w2        = azurerm_virtual_network.w2.address_space[0]
+  }
+  name                   = each.key
+  resource_group_name    = azurerm_resource_group.rg.name
+  route_table_name       = azurerm_route_table.all_via_ewo.name
+  address_prefix         = each.value
   next_hop_type          = "VirtualAppliance"
   next_hop_in_ip_address = var.ewo_int_lb
 }
 
-resource "azurerm_route" "all_appgw_int" {
-  name                   = "appgw_int"
+resource "azurerm_route_table" "appgw_int_2" {
+  name                = "${var.name}-appgw_int_2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_route" "appgw_int_2-via_inbound" {
+  for_each = {
+    appgw_ext = azurerm_virtual_network.appgw_ext.address_space[0]
+  }
+  name                   = each.key
   resource_group_name    = azurerm_resource_group.rg.name
-  route_table_name       = azurerm_route_table.all.name
-  address_prefix         = azurerm_virtual_network.appgw_int.address_space[0]
+  route_table_name       = azurerm_route_table.appgw_int_2.name
+  address_prefix         = each.value
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = var.inb_int_lb
+}
+
+resource "azurerm_route" "appgw_int_2-via_ewo" {
+  for_each = {
+    appgw_int = azurerm_virtual_network.appgw_int.address_space[0]
+    w1        = azurerm_virtual_network.w1.address_space[0]
+    w2        = azurerm_virtual_network.w2.address_space[0]
+  }
+  name                   = each.key
+  resource_group_name    = azurerm_resource_group.rg.name
+  route_table_name       = azurerm_route_table.appgw_int_2.name
+  address_prefix         = each.value
   next_hop_type          = "VirtualAppliance"
   next_hop_in_ip_address = var.ewo_int_lb
 }
 
-resource "azurerm_route" "all_w1" {
-  name                   = "w1"
-  resource_group_name    = azurerm_resource_group.rg.name
-  route_table_name       = azurerm_route_table.all.name
-  address_prefix         = azurerm_virtual_network.w1.address_space[0]
-  next_hop_type          = "VirtualAppliance"
-  next_hop_in_ip_address = var.ewo_int_lb
-}
-
-resource "azurerm_route" "all_w2" {
-  name                   = "w2"
-  resource_group_name    = azurerm_resource_group.rg.name
-  route_table_name       = azurerm_route_table.all.name
-  address_prefix         = azurerm_virtual_network.w2.address_space[0]
-  next_hop_type          = "VirtualAppliance"
-  next_hop_in_ip_address = var.ewo_int_lb
-}
 
 
 
 resource "azurerm_subnet_route_table_association" "appgw_ext" {
   subnet_id      = azurerm_subnet.appgw_ext_s1.id
-  route_table_id = azurerm_route_table.all.id
+  route_table_id = azurerm_route_table.all_via_inbound.id
 }
 resource "azurerm_subnet_route_table_association" "appgw_int_s1" {
   subnet_id      = azurerm_subnet.appgw_int_s1.id
-  route_table_id = azurerm_route_table.all.id
+  route_table_id = azurerm_route_table.all_via_ewo.id
 }
 resource "azurerm_subnet_route_table_association" "appgw_int_s2" {
   subnet_id      = azurerm_subnet.appgw_int_s2.id
-  route_table_id = azurerm_route_table.all.id
+  route_table_id = azurerm_route_table.appgw_int_2.id
 }
 resource "azurerm_subnet_route_table_association" "w1" {
   subnet_id      = azurerm_subnet.w1_s1.id
-  route_table_id = azurerm_route_table.all.id
+  route_table_id = azurerm_route_table.all_via_ewo.id
 }
-
-
-resource "azurerm_route_table" "appgw_ext" {
-  name                = "${var.name}-appgw-ext"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-}
-
-resource "azurerm_route" "appgw_ext-int" {
-  name                   = "to-int-appgw"
-  resource_group_name    = azurerm_resource_group.rg.name
-  route_table_name       = azurerm_route_table.appgw_ext.name
-  address_prefix         = "172.29.0.0/24"
-  next_hop_type          = "VirtualAppliance"
-  next_hop_in_ip_address = var.ewo_int_lb
-}
-
-/*
-resource "azurerm_subnet_route_table_association" "appgw_ext" {
-  subnet_id      = azurerm_subnet.appgw_ext_s1.id
-  route_table_id = azurerm_route_table.appgw_ext.id
-}
-*/
-
-
-resource "azurerm_route_table" "appgw_int" {
-  name                = "${var.name}-appgw-int"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-}
-
-resource "azurerm_route" "appgw_int-ext" {
-  name                   = "to-ext-appgw"
-  resource_group_name    = azurerm_resource_group.rg.name
-  route_table_name       = azurerm_route_table.appgw_int.name
-  address_prefix         = "192.168.0.0/22"
-  next_hop_type          = "VirtualAppliance"
-  next_hop_in_ip_address = var.ewo_int_lb
-}
-
-/*
-resource "azurerm_subnet_route_table_association" "appgw_int" {
-  subnet_id      = azurerm_subnet.appgw_int_s2.id
-  route_table_id = azurerm_route_table.appgw_int.id
-}
-*/
-
