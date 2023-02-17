5205e5437c4f7337f3f52eacaca85eacc35cc44a
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-02-17 17:24:51 +0100

    fix: plugin: correct nsg association

--- plugin/hosts.tf
+++ plugin/hosts.tf
@@ -8,6 +8,7 @@ module "h1" {
   private_ip_address  = cidrhost(azurerm_subnet.w1_s1.address_prefixes[0], 5)
   password            = var.password
   public_key          = azurerm_ssh_public_key.this.public_key
+  associate_nsg       = true
   security_group      = azurerm_network_security_group.hosts.id
 }
 
@@ -21,5 +22,6 @@ module "h2" {
   private_ip_address  = cidrhost(azurerm_subnet.w2_s1.address_prefixes[0], 5)
   password            = var.password
   public_key          = azurerm_ssh_public_key.this.public_key
+  associate_nsg       = true
   security_group      = azurerm_network_security_group.hosts.id
 }
