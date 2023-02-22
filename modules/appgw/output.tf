f12a26f0e729e47ce10d998485ffc2f38dd7670b
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-02-22 14:51:26 +0100

    feat: modules/appgw: output public ip

--- modules/appgw/output.tf
+++ modules/appgw/output.tf
@@ -1,3 +1,7 @@
 output "id" {
   value = var.managed_by_agic ? azurerm_application_gateway.appgw_agic[0].id : azurerm_application_gateway.appgw_regular[0].id
 }
+
+output "public_ip_address" {
+  value = var.tier == "Standard_v2" ? azurerm_public_ip.this[0].ip_address : null
+}
