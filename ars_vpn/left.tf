2b37a17a00e0442ecdfd9d1e8e2bc31e61c07af0
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-02-26 21:10:38 +0100

    feat: ars_vpn: add left-u left-b global peering

---
+++ ars_vpn/left.tf
@@ -0,0 +1,67 @@
+resource "azurerm_virtual_network_peering" "vnet_left_u_hub-vnet_left_b_hub" {
+  name                      = "left-u-hub--left-b-hub"
+  resource_group_name       = azurerm_resource_group.rg1.name
+  virtual_network_name      = module.vnet_left_u_hub.vnet.name
+  remote_virtual_network_id = module.vnet_left_b_hub.vnet.id
+  allow_forwarded_traffic   = true
+}
+
+resource "azurerm_virtual_network_peering" "vnet_left_b_hub-vnet_left_u_hub" {
+  name                      = "left-b-hub--left-u-hub"
+  resource_group_name       = azurerm_resource_group.rg2.name
+  virtual_network_name      = module.vnet_left_b_hub.vnet.name
+  remote_virtual_network_id = module.vnet_left_u_hub.vnet.id
+  allow_forwarded_traffic   = true
+  depends_on = [
+    azurerm_virtual_network_peering.vnet_left_u_hub-vnet_left_b_hub
+  ]
+}
+
+
+resource "azurerm_route_table" "left_u_hub" {
+  name                = "${var.name}-left_u_hub"
+  resource_group_name = azurerm_resource_group.rg1.name
+  location            = azurerm_resource_group.rg1.location
+}
+
+resource "azurerm_route" "left_u_hub" {
+  for_each = {
+    srv1 = module.vnet_left_b_srv_1.vnet.address_space[0]
+  }
+  name                   = each.key
+  resource_group_name    = azurerm_resource_group.rg1.name
+  route_table_name       = azurerm_route_table.left_u_hub.name
+  address_prefix         = each.value
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = local.private_ips.left_b_hub_ilb["obew"]
+}
+
+resource "azurerm_subnet_route_table_association" "left_u_hub" {
+  subnet_id      = module.vnet_left_u_hub.subnets["data"].id
+  route_table_id = azurerm_route_table.left_u_hub.id
+}
+
+
+resource "azurerm_route_table" "left_b_hub" {
+  name                = "${var.name}-left_b_hub"
+  resource_group_name = azurerm_resource_group.rg2.name
+  location            = azurerm_resource_group.rg2.location
+}
+
+resource "azurerm_route" "left_b_hub" {
+  for_each = {
+    srv1 = module.vnet_left_u_srv_1.vnet.address_space[0]
+    sdgw = "10.0.0.0/8"
+  }
+  name                   = each.key
+  resource_group_name    = azurerm_resource_group.rg2.name
+  route_table_name       = azurerm_route_table.left_b_hub.name
+  address_prefix         = each.value
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = local.private_ips.left_u_hub_ilb["obew"]
+}
+
+resource "azurerm_subnet_route_table_association" "left_b_hub" {
+  subnet_id      = module.vnet_left_b_hub.subnets["data"].id
+  route_table_id = azurerm_route_table.left_b_hub.id
+}
