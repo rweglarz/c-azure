07a0263d9fd2c706dc7113d5f03a640279ad54cd
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-07-28 07:45:15 +0200

    feat: modules/vnet: outptut vnet name and id directly

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,3 +1,11 @@
+output "name" {
+  value = azurerm_virtual_network.this.name
+}
+
+output "id" {
+  value = azurerm_virtual_network.this.id
+}
+
 output "vnet" {
   value = azurerm_virtual_network.this
 }
