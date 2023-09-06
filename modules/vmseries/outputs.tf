91c366be7b873b12989a37b557c9cf9266af0f14
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-09-06 18:21:22 +0200

    feat: modules/vmseries: output private_ip_list like aws module do

--- modules/vmseries/outputs.tf
+++ modules/vmseries/outputs.tf
@@ -6,6 +6,10 @@ output "public_ips" {
   value = { for k, v in azurerm_public_ip.this: k => v.ip_address}
 }
 
+output "private_ip_list" {
+  value = { for k, v in azurerm_network_interface.this : k => [v.ip_configuration[0].private_ip_address] }
+}
+
 output "interfaces" {
   value = local.interfaces
 }
