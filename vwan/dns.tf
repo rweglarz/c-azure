11219274df400633a58a99b4ca98bda5e1db874f
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-10-20 10:15:50 +0200

    chore: vwan: wrap up external dns resources

--- vwan/dns.tf
+++ vwan/dns.tf
@@ -8,174 +8,32 @@ resource "azurerm_dns_a_record" "aws-fw-1" {
   ]
 }
 
-resource "azurerm_dns_a_record" "hub1_sec_fw" {
-  name                = "vwan-hub1-sec-fw"
+resource "azurerm_dns_a_record" "public" {
+  for_each = {
+    vwan-hub1-sec-fw     = module.hub1_sec_fw.mgmt_ip_address
+    vwan-hub1-sec-spoke1 = module.hub1_sec_spoke1_h.public_ip
+    vwan-hub2-spoke1-s1  = module.hub2_spoke1_s1_h.public_ip
+    vwan-hub2-spoke1-s2  = module.hub2_spoke1_s2_h.public_ip
+    vwan-hub2-spoke2     = module.hub2_spoke2_h.public_ip
+    vwan-hub4-spoke1-s1  = module.hub4_spoke1_s1_h.public_ip
+    vwan-hub4-spoke1-s2  = module.hub4_spoke1_s2_h.public_ip
+    vwan-hub4-spoke2-prv = module.hub4_spoke2_h_prv.public_ip
+    vwan-hub2-sdwan-fw   = module.hub2_sdwan_fw.mgmt_ip_address
+    vwan-hub4-sdwan-fw   = module.hub4_sdwan_fw.mgmt_ip_address
+    vwan-sdwan-spoke1-fw = module.sdwan_spoke1_fw.mgmt_ip_address
+    vwan-sdwan-spoke1    = module.sdwan_spoke1_h.public_ip,
+    vwan-ipsec-hub2-fw1  = module.ipsec_hub2_fw1.mgmt_ip_address
+    vwan-ipsec-hub2-fw2  = module.ipsec_hub2_fw2.mgmt_ip_address
+    vwan-ipsec-spoke1-fw = module.ipsec_spoke1_fw.mgmt_ip_address
+    vwan-ipsec-hubs      = local.public_ip["ipsec_hub2_fw1"][0]
+    vwan-hub4-ext-lb     = azurerm_public_ip.hub4_ext_lb.ip_address
+  }
+  name = each.key
   resource_group_name = var.dns_zone_rg
   zone_name           = var.dns_zone_name
   ttl                 = local.dns_ttl
   records = [
-    module.hub1_sec_fw.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub1_sec_spoke1_h" {
-  name                = "vwan-hub1-sec-spoke1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub1_sec_spoke1_h.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub2_spoke1_s1_h" {
-  name                = "vwan-hub2-spoke1-s1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub2_spoke1_s1_h.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub2_spoke1_s2_h" {
-  name                = "vwan-hub2-spoke1-s2"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub2_spoke1_s2_h.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub2_spoke2_h" {
-  name                = "vwan-hub2-spoke2"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub2_spoke2_h.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub4_spoke1_s1_h" {
-  name                = "vwan-hub4-spoke1-s1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub4_spoke1_s1_h.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub4_spoke1_s2_h" {
-  name                = "vwan-hub4-spoke1-s2"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub4_spoke1_s2_h.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub4_spoke2_h_prv" {
-  name                = "vwan-hub4-spoke2-prv"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub4_spoke2_h_prv.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub2_sdwan_fw" {
-  name                = "vwan-hub2-sdwan-fw"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub2_sdwan_fw.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub4_sdwan_fw" {
-  name                = "vwan-hub4-sdwan-fw"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.hub4_sdwan_fw.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "sdwan_spoke1_fw" {
-  name                = "vwan-sdwan-spoke1-fw"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.sdwan_spoke1_fw.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "sdwan_spoke1_h" {
-  name                = "vwan-sdwan-spoke1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.sdwan_spoke1_h.public_ip,
-  ]
-}
-
-resource "azurerm_dns_a_record" "ipsec_hub2_fw1" {
-  name                = "vwan-ipsec-hub2-fw1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.ipsec_hub2_fw1.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "ipsec_hub2_fw2" {
-  name                = "vwan-ipsec-hub2-fw2"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.ipsec_hub2_fw2.mgmt_ip_address
-  ]
-}
-
-
-resource "azurerm_dns_a_record" "ipsec_spoke1_fw" {
-  name                = "vwan-ipsec-spoke1-fw"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = local.dns_ttl
-  records = [
-    module.ipsec_spoke1_fw.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "ipsec_hubs" {
-  name                = "vwan-ipsec-hubs"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 60
-  records = [
-    local.public_ip["ipsec_hub2_fw1"][0]
-  ]
-}
-
-resource "azurerm_dns_a_record" "hub4_ext_lb" {
-  name                = "vwan-hub4-ext-lb"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 60
-  records = [
-    azurerm_public_ip.hub4_ext_lb.ip_address
+    each.value
   ]
 }
 
