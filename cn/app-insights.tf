c97807f771b4be2d375b6e297eab5a14f28bf98f
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2024-03-28 18:09:01 +0100

    feat: cn: move to log analytics workspace

--- cn/app-insights.tf
+++ cn/app-insights.tf
@@ -1,7 +1,16 @@
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "${var.name}-log-analytics"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 resource "azurerm_application_insights" "this" {
   name                = "${var.name}-app-insights"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.this.id
   application_type    = "other"
 }
 
