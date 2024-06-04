0e9028f3da2a2166d846fd256f8fb0ce50b5059d
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2024-06-04 22:07:38 +0200

    chore: ars_vpn: collapse dns resources

--- ars_vpn/dns.tf
+++ ars_vpn/dns.tf
@@ -1,113 +1,23 @@
-resource "azurerm_dns_a_record" "left-u-hub-fw" {
-  name                = "ars-left-u-hub-fw"
+resource "azurerm_dns_a_record" "this" {
+  for_each = {
+    ars-left-u-hub-fw    = module.left_u_hub_fw["1"].mgmt_ip_address
+    ars-left-u-ipsec-fw1 = module.left_u_ipsec_fw1.mgmt_ip_address
+    ars-left-u-ipsec-fw2 = module.left_u_ipsec_fw2.mgmt_ip_address
+    ars-left-b-hub-fw    = module.left_b_hub_fw["1"].mgmt_ip_address
+    ars-left-b-ipsec-fw1 = module.left_b_ipsec_fw1.mgmt_ip_address
+    ars-left-b-ipsec-fw2 = module.left_b_ipsec_fw2.mgmt_ip_address
+    ars-right-hub-fw     = module.right_hub_fw.mgmt_ip_address
+    ars-right-env-fw1    = module.right_env_fw1.mgmt_ip_address
+    ars-left-u-srv11     = module.linux_left_u_srv11.public_ip
+    ars-left-b-srv11     = module.linux_left_b_srv11.public_ip
+    ars-right-srv11      = module.linux_right_srv11.public_ip
+  }
+  name                = each.key
   resource_group_name = var.dns_zone_rg
   zone_name           = var.dns_zone_name
   ttl                 = 300
   records = [
-    module.left_u_hub_fw["1"].mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "left-u-ipsec-fw1" {
-  name                = "ars-left-u-ipsec-fw1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.left_u_ipsec_fw1.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "left-u-ipsec-fw2" {
-  name                = "ars-left-u-ipsec-fw2"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.left_u_ipsec_fw2.mgmt_ip_address
-  ]
-}
-
-
-resource "azurerm_dns_a_record" "left-b-hub-fw" {
-  name                = "ars-left-b-hub-fw"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.left_b_hub_fw["1"].mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "left-b-ipsec-fw1" {
-  name                = "ars-left-b-ipsec-fw1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.left_b_ipsec_fw1.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "left-b-ipsec-fw2" {
-  name                = "ars-left-b-ipsec-fw2"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.left_b_ipsec_fw2.mgmt_ip_address
-  ]
-}
-
-
-resource "azurerm_dns_a_record" "right-hub-fw" {
-  name                = "ars-right-hub-fw"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.right_hub_fw.mgmt_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "right-env-fw1" {
-  name                = "ars-right-env-fw1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.right_env_fw1.mgmt_ip_address
-  ]
-}
-
-
-resource "azurerm_dns_a_record" "left_u_srv11" {
-  name                = "ars-left-u-srv11"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.linux_left_u_srv11.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "left_b_srv11" {
-  name                = "ars-left-b-srv11"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.linux_left_b_srv11.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "right_srv11" {
-  name                = "ars-right-srv11"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 300
-  records = [
-    module.linux_right_srv11.public_ip
+    each.value
   ]
 }
 
