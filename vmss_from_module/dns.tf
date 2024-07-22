e5a723fb5d37df7d08e3c94d20e14d40943e4fc4
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2024-07-22 18:04:55 +0200

    chore: vmss_from_module: roll up dns record creation

--- vmss_from_module/dns.tf
+++ vmss_from_module/dns.tf
@@ -1,69 +1,18 @@
-resource "azurerm_dns_a_record" "jumphost" {
-  name                = "vmss-m-jumphost"
+resource "azurerm_dns_a_record" "public" {
+  for_each = {
+    jumphost = module.jumphost.public_ip
+    app11    = module.srv_app11.public_ip
+    app12    = module.srv_app12.public_ip
+    app2     = module.srv_app2.public_ip
+    db1      = module.srv_db1.public_ip
+    appgw1   = module.appgw1.public_ip_address
+    appgw2   = module.appgw2.public_ip_address
+  }
+  name                = "vmss-m-${each.key}"
   resource_group_name = var.dns_zone_rg
   zone_name           = var.dns_zone_name
   ttl                 = 60
   records = [
-    module.jumphost.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "app11" {
-  name                = "vmss-m-app11"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 60
-  records = [
-    module.srv_app11.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "app12" {
-  name                = "vmss-m-app12"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 60
-  records = [
-    module.srv_app12.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "db1" {
-  name                = "vmss-m-db1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 60
-  records = [
-    module.srv_db1.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "app2" {
-  name                = "vmss-m-app2"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 60
-  records = [
-    module.srv_app2.public_ip
-  ]
-}
-
-resource "azurerm_dns_a_record" "appgw1" {
-  name                = "vmss-m-appgw1"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 60
-  records = [
-    module.appgw1.public_ip_address
-  ]
-}
-
-resource "azurerm_dns_a_record" "appgw2" {
-  name                = "vmss-m-appgw2"
-  resource_group_name = var.dns_zone_rg
-  zone_name           = var.dns_zone_name
-  ttl                 = 60
-  records = [
-    module.appgw2.public_ip_address
+    each.value
   ]
 }
